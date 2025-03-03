ebf49507d344e594e85c65d556dffec48288e1e0
konodop <150059168+konodop@users.noreply.github.com>
2025-03-03 20:45:15 +0300

    starting agent in gorutine

--- cmd/main.go
+++ cmd/main.go
@@ -2,9 +2,12 @@ package main
 
 import (
 	"calc/internal/application"
+	"calc/pkg/agent"
 )
 
 func main() {
 	app := application.New()
+	Agent := agent.New()
+	go Agent.RunAgent()
 	app.RunServer()
 }
